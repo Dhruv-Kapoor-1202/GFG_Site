3f84314d2a4595975f9af6dcf5236f0b1246b34c
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2023-04-15 23:56:58 +0530

    First Commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/resource.css';
+import '../styles/components/appoint.css';
+import '../styles/components/footer.css';
+
+
+
+
+import '../styles/utils.css';
